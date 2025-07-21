e75bfe04bd730130bc1d8bdbe5d97fa4b1466708
Jenish bhalani <jenishbhalani18@gmail.com>
2025-07-21 14:03:20 +0530

    Delete prg12.py

--- prg12.py
+++
@@ -1,8 +0,0 @@
-#create list with different name and used membership operator
-names = ["jenish","bhaumik","ankit","dev","vasu"]
-nm = input("Enter any name : ")
-
-if nm in names:
-    print("it does exist in the list")
-else:
-    print("it doesn't exist in the list")
