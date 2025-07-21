d565fbffeb190d4454dfdf668e9b8aa46e47cdbe
Jenish bhalani <jenishbhalani18@gmail.com>
2025-07-21 14:03:27 +0530

    Delete prg13.py

--- prg13.py
+++
@@ -1,7 +0,0 @@
-#input number from user and check if it's even or not
-n = int(input("enter any number : "))
-if n % 2 == 0:
-    print("it's an even number")
-else:
-    print("it's not an even number")
-    
