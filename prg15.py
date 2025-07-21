62428a80e2ff836bdc14b1c115611e00db15ea67
Jenish bhalani <jenishbhalani18@gmail.com>
2025-07-21 14:03:52 +0530

    Delete prg15.py

--- prg15.py
+++
@@ -1,9 +0,0 @@
-#input username and password from use and check username = "bca" and password = "sem-5"
-u = input("Enter your username : ")
-p = input("Enter your password : ")
-
-if u == "bca" and p == "sem-5":
-    print("it does match the username and password")
-else:
-    print("it doesn't match the username and password")
-
