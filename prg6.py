0c4410d223df58a5dd420f4229769b0502ba59c2
Jenish bhalani <jenishbhalani18@gmail.com>
2025-07-21 14:04:06 +0530

    Delete prg6.py

--- prg6.py
+++
@@ -1,9 +0,0 @@
-#create list l1 with items rno,name,city,per
-#change city name 
-#print list
-#print value of 2nd position
-
-l1=[1,"jenish","ahmedabad",85.44]
-l1[2] = "keshod"
-print(l1)
-print(l1[1])
