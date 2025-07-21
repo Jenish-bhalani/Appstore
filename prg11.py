55a576a3b6ab9c24aaa55e872d54f5e6ef8039fd
Jenish bhalani <jenishbhalani18@gmail.com>
2025-07-21 14:03:13 +0530

    Delete prg11.py

--- prg11.py
+++
@@ -1,7 +0,0 @@
-#input your age and check that are you eligible for voting or not
-age = int(input("Enter your age : "))
-if age >= 18:
-    print("you are eligible for voting...")
-else:
-    print("you are not eligible for voting...")
-    
